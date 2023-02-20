74f248383b11d50e8c6b7e08de5292e88d18b9ad
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-02-20 19:04:33 +0700

    change favicon, change description

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,77 +1,80 @@
 <!DOCTYPE html>
-<!--
-Template Name: NobleUI - Laravel Admin Dashboard Template
-Author: NobleUI
-Website: https://www.nobleui.com
-Portfolio: https://themeforest.net/user/nobleui/portfolio
-Contact: nobleui123@gmail.com
-Purchase: https://1.envato.market/nobleui_laravel
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
 <html>
-<head>
-  <meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="Responsive Laravel Admin Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="NobleUI">
-	<meta name="keywords" content="nobleui, bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, laravel, theme, front-end, ui kit, web">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Andev Dashboard">
+    <meta name="author" content="Fahmi Arif">
+    <meta name="keywords" content="bootstrap, bootstrap 5, bootstrap5, admin, dashboard, template, responsive, css, sass, html, laravel, theme, front-end, ui kit, web">
 
-  <title>NobleUI - Laravel Admin Dashboard Template</title>
+    <title>Andev Dashboard</title>
 
-  <!-- Fonts -->
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
-  <!-- End fonts -->
-  
-  <!-- CSRF Token -->
-  <meta name="_token" content="{{ csrf_token() }}">
-  
-  <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+    <!-- End fonts -->
+    
+    <!-- CSRF Token -->
+    <meta name="_token" content="{{ csrf_token() }}">
+    
+    {{-- <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}"> --}}
 
-  <!-- plugin css -->
-  <link href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}" rel="stylesheet" />
-  <link href="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" />
-  <!-- end plugin css -->
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('ico/apple-touch-icon-57x57.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('ico/apple-touch-icon-114x114.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('ico/apple-touch-icon-72x72.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="{{ asset('ico/apple-touch-icon-144x144.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ asset('ico/apple-touch-icon-60x60.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ asset('ico/apple-touch-icon-120x120.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ asset('ico/apple-touch-icon-76x76.png') }}" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('ico/apple-touch-icon-152x152.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('ico/favicon-196x196.png') }}" sizes="196x196" />
+    <link rel="icon" type="image/png" href="{{ asset('ico/favicon-96x96.png') }}" sizes="96x96" />
+    <link rel="icon" type="image/png" href="{{ asset('ico/favicon-32x32.png') }}" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ asset('ico/favicon-16x16.png') }}" sizes="16x16" />
+    <link rel="icon" type="image/png" href="{{ asset('ico/favicon-128.png') }}" sizes="128x128" />
 
-  @stack('plugin-styles')
+    <!-- plugin css -->
+    <link href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" />
+    <!-- end plugin css -->
 
-  <!-- common css -->
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
-  <!-- end common css -->
+    @stack('plugin-styles')
 
-  @stack('style')
-</head>
-<body data-base-url="{{url('/')}}">
+    <!-- common css -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+    <!-- end common css -->
 
-  <script src="{{ asset('assets/js/spinner.js') }}"></script>
+    @stack('style')
+  </head>
+  <body data-base-url="{{url('/')}}">
 
-  <div class="main-wrapper" id="app">
-    @include('layout.sidebar')
-    <div class="page-wrapper">
-      @include('layout.header')
-      <div class="page-content">
-        @yield('content')
+    <script src="{{ asset('assets/js/spinner.js') }}"></script>
+
+    <div class="main-wrapper" id="app">
+      @include('layout.sidebar')
+      <div class="page-wrapper">
+        @include('layout.header')
+        <div class="page-content">
+          @yield('content')
+        </div>
+        @include('layout.footer')
       </div>
-      @include('layout.footer')
     </div>
-  </div>
 
-    <!-- base js -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('assets/plugins/feather-icons/feather.min.js') }}"></script>
-    <script src="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
-    <!-- end base js -->
+      <!-- base js -->
+      <script src="{{ asset('js/app.js') }}"></script>
+      <script src="{{ asset('assets/plugins/feather-icons/feather.min.js') }}"></script>
+      <script src="{{ asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+      <!-- end base js -->
 
-    <!-- plugin js -->
-    @stack('plugin-scripts')
-    <!-- end plugin js -->
+      <!-- plugin js -->
+      @stack('plugin-scripts')
+      <!-- end plugin js -->
 
-    <!-- common js -->
-    <script src="{{ asset('assets/js/template.js') }}"></script>
-    <!-- end common js -->
+      <!-- common js -->
+      <script src="{{ asset('assets/js/template.js') }}"></script>
+      <!-- end common js -->
 
-    @stack('custom-scripts')
-</body>
+      @stack('custom-scripts')
+  </body>
 </html>
